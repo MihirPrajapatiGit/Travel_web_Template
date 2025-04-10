994061bec834584ce2e83a8094f99ab164b2c2bc
number1DataScientist <prajapatimihir502@gmail.com>
2025-04-10 12:25:28 +0530

    Project done

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+          <Routes>
+            <Route path='/' exact Component= {Home}/>
+            <Route path='/services' exact Component= {Services}/>
+            <Route path='/products' exact Component= {Products}/>
+            <Route path='/sign-up' exact Component= {SignUp}/>
+          </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
+
+
